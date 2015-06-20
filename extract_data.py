e5ad84769ffdc34a964dae817caf2df5afaf6d04
upaang <upaang.saxena@minjar.com>
2015-06-21 03:02:49 +0530

    Table  converted successfully to JSON

--- extract_data.py
+++ extract_data.py
@@ -1,7 +1,8 @@
 __author__ = 'minjar86'
-import json
 import urllib2
+
 from BeautifulSoup import BeautifulSoup
+
 
 url='http://www.airtel.in/mobile/prepaid/recharge-online?CIRCLE=2&CIRCLENAME=Delhi%20NCR&tab=1#tabs-1'
 request=urllib2.Request(url=url)
@@ -13,18 +14,39 @@ split_page=content.split("<div class=\"collapse shown\">",1)
 s=split_page[1].split("</div>",1)
 # print s[0]
 count=0
-data_map={}
+data_map = []
+obj_data = {}
 for row in BeautifulSoup(s[0])("tr"):
-    print row
+    # print row
     if count==0:
         for cell in row("td"):
             # print cell.text
-            count+=1
-            data_map[str(cell.text)]=""
-        print data_map
-    else:
+            obj_data[str(cell.text)] = ""
+        count += 1
+        print obj_data
+        print(count)
+    elif count < 5:
+        val = 0
         for cell in row("td"):
-            val=0
+            # print cell.text
             if val==0:
-                data_map["Denomination(Range)"]=cell.text
-            print cell.text
+                obj_data["Denomination(Range)"] = str(cell.text)
+            if val == 1:
+                obj_data["TalkTime Range Value (Rs)"] = str(cell.text)
+            if val == 2:
+                obj_data["Processing Fee (Rs)"] = str(cell.text)
+            if val == 3:
+                obj_data["Service tax (Rs)"] = str(cell.text)
+            if val == 4:
+                obj_data["Validity (days)"] = str(cell.text)
+            if val == 5:
+                obj_data["Category"] = str(cell.text)
+            val += 1
+            # val=0
+            # print cell.text
+            # print obj_data
+            data_map.append(obj_data)
+        count += 1
+        # print obj_data
+
+print data_map
